2a6edaaee3ed5f5cdbe537b992babbf12c712c30
Mattia Crovero <mcrovero@gmail.com>
2025-09-06 12:56:25 +0200

    updated

--- app/actions.ts
+++ app/actions.ts
@@ -1,11 +1,21 @@
 "use server";
 
-import { Effect } from "effect";
-
 async function getData(input: number) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   return { data: "done " + input };
 }
+
+// This is not a valid Server Action
+export const doSomethingPromise = (input: number) => getData(input);
+
+// This is valid
+export const doSomethingPromiseAsync = async (input: number) =>
+  await getData(input);
+
+/**
+ * !! The EffectTs case to allow Promise-returning Server Actions !!
+ **/
+import { Effect } from "effect";
 
 // Simple execution not viable as a Server Action
 export const doSomethingNoWrapper = (input: number) =>
@@ -15,7 +25,8 @@ export const doSomethingNoWrapper = (input: number) =>
 export const doSomethingNoWrapperAync = async (input: number) =>
   Effect.promise(() => getData(input)).pipe(Effect.runPromise);
 
-// Using wrapper function
+// Most of the time we run effects using a wrapper function
+// There is no developer friendly way to run the wrapper with the async workaround
 
 function runEffectAction<I, O, E>(
   effectFn: (args: I) => Effect.Effect<O, E, never>
